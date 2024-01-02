4c29ffd85deac4704be67c3c1e9700a2d9d31f54
Mrosa90 <145835544+Mrosa90@users.noreply.github.com>
2024-01-02 18:19:30 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console! ");
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let who = ["El perro", "Mi abuelo", "Tu gato", "Mi cartera"];
+  let action = ["muerde", "corre", "salta", "rompe"];
+  let what = ["el coche", "las llaves", "el collar"];
+  let when = [
+    "despues de comer",
+    "cuando corre",
+    "despues de ir al baño",
+    "durante la comida",
+    "mientras compra"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whenIndx]
+  );
 };
